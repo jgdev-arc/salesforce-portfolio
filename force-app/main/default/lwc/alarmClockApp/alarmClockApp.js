2456eaf52ea02b4908e5a0db501f04e9b85b0fac
TheLordZedd <pcgpod@yahoo.com>
2023-11-13 23:55:02 -0600

    Alarm clock initial setup

---
+++ force-app/main/default/lwc/alarmClockApp/alarmClockApp.js
@@ -0,0 +1,55 @@
+import { LightningElement } from 'lwc';
+import AlarmClockAssets from '@salesforce/resourceUrl/AlarmClockAssets'
+export default class AlarmClockApp extends LightningElement {
+    clockImage = AlarmClockAssets + '/AlarmClockAssets/clock.png'
+    currentTime = ''
+    hours = []
+    minutes = []
+    meridiems = ['AM', 'PM']
+
+    connectedCallback() {
+        this.currentTimeHandler()
+        this.createHoursOptions()
+    }
+    currentTimeHandler() {
+        setInterval(() => {
+        let dateTime = new Date()
+        let hour = dateTime.getHours()
+        let min = dateTime.getMinutes()
+        let sec = dateTime.getSeconds()
+        let ampm = "AM"
+
+        if (hour == 0) {
+            hour = 12
+            ampm = "AM" 
+        } else if (hour == 12) {
+            ampm = "PM"
+        } else if (hour >= 12) {
+            hour = hour - 12
+            ampm = "PM"
+        }
+
+        hour = hour < 10 ? "0" + hour : hour
+        min = min < 10 ? "0" + min : min
+        sec = sec < 10 ? "0" + sec : sec
+
+        this.currentTime = `${hour}:${min}:${sec} ${ampm}`
+        }, 1000);
+
+    }
+
+    createHoursOptions() {
+        for(let i = 1; i < 12; i++) {
+            let val = i < 10 ? `0${i}` : i
+            this.hours.push(val) 
+        }
+    }
+
+    createMinutesOptions() {
+        for(let i = 0; i < 59; i++) {
+            let val = i < 10 ? `0${i}` : i
+            this.minutes.push(val) 
+        }
+    }
+
+}
